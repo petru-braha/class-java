8b4f1dd6058d8610ed23d93cafec48f251d04782
p <petrubraha@gmail.com>
2025-03-19 08:31:13 +0200

    fixed syntax errors homework 3;

--- lab3/Solution.java
+++ lab3/Solution.java
@@ -1,5 +1,6 @@
 package lab3;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.TreeSet;
 import lab3.airfield.Flight;
@@ -22,12 +23,19 @@ public class Solution {
     if (-1 != isAssigned)
       return -1;
 
-    int[] runways = new int[countRunways];
+    HashMap<Flight, Integer> runways = new HashMap<Flight, Integer>();
 
     for (Flight it0 : schedule) {
-      HashSet colors = new HashSet<>();
-      for(Flight neighbor : it0.getConflicts())
-        if
+      HashSet<Integer> colors = new HashSet<>();
+      for (Flight neighbor : it0.getConflicts())
+        if (runways.containsKey(neighbor))
+          colors.add(runways.get(neighbor));
+
+      for (Integer i = 0; i < schedule.size(); i++)
+        if (false == colors.contains(i)) {
+          runways.put(it0, i);
+          break;
+        }
     }
 
     return -1;
