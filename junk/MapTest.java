4831a1519fd8cacecdf1c8910acf1dc5a8f6aa1a
p <petrubraha@gmail.com>
2025-03-23 17:55:23 +0200

    test;

--- junk/MapTest.java
+++ junk/MapTest.java
@@ -1,10 +1,18 @@
 package junk;
 
-import java.util.TreeSet;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class MapTest {
 
   public static void main(String[] args) {
+
+    var numbers = Arrays.asList(1, 2, 3, 4, 5);
+    List<String> dada = numbers.stream()
+        .map(x -> "number: " + Integer.toString(x) + ";")
+        .collect(Collectors.toList());
+    System.out.println(dada);
   }
-  
+
 }
